6de09c5166755f41549c63beac27d30e8f3303a5
UMU <umu618@hotmail.com>
2023-01-20 01:00:15 +0800

    + res

---
+++ src/PowerEconomizer/PowerEconomizer/res/resource.h
@@ -0,0 +1,39 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PowerEconomizer.rc
+//
+#define IDR_MAINFRAME                   100
+#define IDD_MAINDIALOG                  101
+#define IDS_YES                         104
+#define IDS_NO                          105
+#define IDC_EDIT_OS_VERSION             1000
+#define IDC_EDIT_IS_ADMIN               1001
+#define IDC_CHECK_ECO                   1002
+#define IDC_CHECK_BLUR                  1003
+#define IDC_COMBO_WAIT                  1004
+#define IDC_SYSLINK_FEEDBACK            1005
+#define IDC_SYSLINK_ABOUT               1006
+#define IDC_BUTTON_VIEW_LOG             1007
+#define IDC_BUTTON_FREE_CONSOLE         1008
+#define IDC_ELEVATE                     1009
+#define ID_SHELL_OPEN                   40001
+#define ID_BLUR_ACRYLIC_BLUR            40002
+#define ID_BLUR_BLUR                    40003
+#define ID_BLUR_PAUSE_5                 40004
+#define ID_BLUR_PAUSE_10                40005
+#define ID_BLUR_PAUSE_15                40006
+#define ID_BLUR_PAUSE_30                40007
+#define ID_BLUR_PAUSE_40                40008
+#define ID_BLUR_PAUSE_60                40009
+#define ID_BLUR_PAUSE_120               40010
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           102
+#endif
+#endif
